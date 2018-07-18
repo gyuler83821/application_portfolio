62355bb399f177023b64a6edbe86decbbca92924
gyuler83821 <gyuler83821@gmail.com>
2018-07-18 09:54:57 +0800

    Add files via upload

---
+++ Invoice/final.h
@@ -0,0 +1,12 @@
+#define H20 3
+#define E200 3
+#define MAX 2000
+#include <stdio.h>
+char s1000w[9]="26638985";
+char s200w[9]="37266877";
+char h20w[H20][9]={"15720791","21230260","55899892"};
+char e200[E200][4]={"248","285","453"};
+char data[MAX][9];
+char tmp[9];
+int bonus;
+FILE* fopen(),*p1,*p2;
